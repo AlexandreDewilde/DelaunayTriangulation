22f7b64a2cc6ff0e9d484915a2da5a30aaa4182e
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2023-12-13 16:48:58 +0100

    add bowyer efficient page

---
+++ scripts/EfficientBowyerWatson.js
@@ -0,0 +1,189 @@
+// based on https://en.wikipedia.org/wiki/Bowyer%E2%80%93Watson_algorithm
+// https://codeforces.com/blog/entry/85638
+
+class EfficientBowyerWatson {
+
+    static Point = class {
+        constructor(idx, x, y) {
+            this.idx = idx;
+            this.x = x;
+            this.y = y;
+        }
+
+        getPointCoord() {
+            return [this.x, this.y];
+        }
+    }
+
+    static Triangle = class {
+        constructor(a, b, c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.circum = computeCircum(this.getPointsCoords());
+            this.radius = computeDst(a.getPointCoord(), this.circum);
+        }
+
+        toString() {
+            const idxs = [this.a.idx, this.b.idx, this.c.idx];
+            idxs.sort();
+            return `${idxs[0]}_${idxs[1]}_${idxs[2]}`;
+        }
+
+
+        isCircum(point) {
+            return computeDst(this.circum, point.getPointCoord()) < this.radius;
+        }
+
+        getPoints() {
+            return [this.a, this.b, this.c];
+        }
+
+        getPointsCoords() {
+            const coords = [];
+            for (const point of this.getPoints()) {
+                coords.push([point.x, point.y]);
+            }
+            return coords;
+        }
+
+        getEdges() {
+            return [[this.a, this.b], [this.b, this.c], [this.c, this.a]];
+        }
+
+        static getEdgeString(edge) {
+            return Math.min(edge[0].idx, edge[1].idx) + "_" + Math.max(edge[1].idx, edge[0].idx);
+        }
+
+        getEdgesString() {
+            const edges = this.getEdges();
+            const res = [];
+            for (const edge of edges) {
+                res.push(this.constructor.getEdgeString(edge));
+            }
+            return res;
+        }
+    }
+
+    constructor(nodes, drawingMethods) {
+
+        this.nodes = nodes;
+        this.drawingMethods = drawingMethods;
+        this.delaunay = null;
+        this.voronoi = null;
+    }
+
+    draw() {
+        for (const node of this.nodes) {
+            this.drawingMethods.drawPoint(node, 5, "black", this.canvas);
+        }
+        if (!this.delaunay) {
+            this.triangulate();
+        }
+        if (!this.voronoi) {
+            this.computeVoronoi();
+        }
+
+        for (const triangle of Object.values(this.delaunay)) {
+            this.drawingMethods.drawEdge(triangle.a.getPointCoord(), triangle.b.getPointCoord(), true);
+            this.drawingMethods.drawEdge(triangle.c.getPointCoord(), triangle.b.getPointCoord(), true);
+            this.drawingMethods.drawEdge(triangle.a.getPointCoord(), triangle.c.getPointCoord(), true);
+        }
+        for (const edge of this.voronoi) {
+            this.drawingMethods.drawEdge(edge[0], edge[1]);
+        }
+    }
+
+    async triangulate(demo=0, random=true) {
+        const n = this.nodes.length;
+        const supA = new this.constructor.Point(n, -10, -10);
+        const supB = new this.constructor.Point(n+1, 20 + 10, -10);
+        const supC = new this.constructor.Point(n+2, -10, 20 + 10);
+
+        const supTriangle = new this.constructor.Triangle(supA, supB, supC);
+        this.delaunay = {};
+        this.delaunay[supTriangle.toString()] = supTriangle;
+
+        if (random) {
+            shuffleArray(this.nodes);
+        }
+
+        for (let i = 0; i < this.nodes.length; i++) {
+            const point = new this.constructor.Point(i, ...this.nodes[i]);
+            this.addPoint(point);
+            if (demo) {
+                this.computeVoronoi();
+                await new Promise(r => setTimeout(r, demo));
+            }
+        }
+
+        for (const triangleString of Object.values(this.delaunay)) {
+            const triangle = this.delaunay[triangleString];
+            for (const point of triangle.getPoints()) {
+                if (point.idx >= this.nodes.length) {
+                    delete this.delaunay[triangleString];
+                    break;
+                }
+            }
+        }
+    }
+
+    addPoint(point) {
+        const badTriangles = [];
+        const sharedEdges = {};
+        for (const triangle of Object.values(this.delaunay)) {
+            if (triangle.isCircum(point)) {
+                badTriangles.push(triangle);
+                for (const edgeString of triangle.getEdgesString()) {
+                    if (!(edgeString in sharedEdges)) {
+                        sharedEdges[edgeString] = 0;
+                    }
+                    sharedEdges[edgeString]++;
+                }
+            }
+        }
+
+        const polygon = [];
+        for (const triangle of badTriangles) {
+            for (const edge of triangle.getEdges()) {
+                const sharedEdge = sharedEdges[this.constructor.Triangle.getEdgeString(edge)];
+                if (sharedEdge == 1) {
+                    polygon.push(edge);
+                }
+            }
+        }
+
+        for (const triangle of badTriangles) {
+            delete this.delaunay[triangle.toString()];
+        }
+
+        for (const edge of polygon) {
+            const newTriangle = new this.constructor.Triangle(edge[0], edge[1], point);
+            this.delaunay[newTriangle.toString()] = newTriangle;
+        }
+    }
+
+    computeVoronoi() {
+        const edgeTriangle = {};
+        for (const triangle of Object.values(this.delaunay)) {
+            for (const edge of triangle.getEdges()) {
+                const edgeString = this.constructor.Triangle.getEdgeString(edge);
+                if (!(edgeString in edgeTriangle)) {
+                    edgeTriangle[edgeString] = [];
+                }
+                edgeTriangle[edgeString].push(triangle);
+            }
+        }
+        this.voronoi = [];
+        for (const triangle of Object.values(this.delaunay)) {
+            for (const edgeString of triangle.getEdgesString(triangle)) {
+                for (const triangle2 of edgeTriangle[edgeString]) {
+                    if (triangle2 == triangle) {
+                        continue;
+                    }
+                    this.voronoi.push([triangle.circum, triangle2.circum]);
+                }
+            }
+        }
+    }
+}
---
+++ scripts/PageEfficientBowyerWatson.js
@@ -0,0 +1,28 @@
+const sectionCanvas = document.getElementById("section-demo");
+let canvasTriangulation = null;
+let triangulation = null;
+
+let demo = null;
+
+recreateCanvas(1000);
+
+const buttonSkipDemo = document.getElementById("button-skip-demo");
+const buttonRestartDemo = document.getElementById("button-restart-demo");
+
+function recreateCanvas(delay) {
+    if (canvasTriangulation) {
+        sectionCanvas.removeChild(canvasTriangulation);
+    }
+    canvasTriangulation = document.createElement("canvas");
+
+    resizeCanvas(canvasTriangulation, Math.min(window.innerWidth, window.innerHeight) - 80, Math.min(window.innerWidth, window.innerHeight) - 80);
+    window.addEventListener("resize", () => resizeCanvas(canvasTriangulation, Math.min(window.innerWidth, window.innerHeight) - 80, Math.min(window.innerWidth, window.innerHeight) - 80));
+
+    triangulation = new Canvas(nodeData, EfficientBowyerWatson, canvasTriangulation);
+    sectionCanvas.appendChild(canvasTriangulation);
+    demo = triangulation.start(delay);
+}
+buttonSkipDemo.addEventListener("click", () => recreateCanvas(0));
+
+buttonRestartDemo.addEventListener("click", () => recreateCanvas(1000));
+
