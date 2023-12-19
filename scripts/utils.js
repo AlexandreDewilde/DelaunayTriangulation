606b2f14e98b33e7ec0bf1a060b12e1c1f187f84
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2023-12-19 16:32:03 +0100

    utils: random color

--- scripts/utils.js
+++ scripts/utils.js
@@ -11,3 +11,21 @@ function shuffleArray(array) {
         [array[c], array[b]] = [array[b], array[c]];
     }
 }
+
+function randomColor() {
+    // Chatgpt generated the following lists...
+    const colors = [
+        "#FF6B6B", "#70D6FF", "#FFD166", "#45A29E", "#FF6347",  // Row 1
+        "#536DFE", "#FFCE56", "#FF9F43", "#7ED6DF", "#FF5E57",  // Row 2
+        "#80DEEA", "#FFB6C1", "#94A6B8", "#FF7F50", "#77B6EA",  // Row 3
+        "#FFD700", "#B19CD9", "#32CD32", "#FF69B4", "#87CEEB",  // Row 4
+        "#FF8C00", "#6495ED", "#DC143C", "#00FA9A", "#9932CC",  // Row 5
+        "#00CED1", "#DAA520", "#8A2BE2", "#008080", "#FF4500",  // Row 6
+        "#00BFFF", "#9370DB", "#48D1CC", "#CD853F", "#4682B4",  // Row 7
+        "#FF1493", "#1E90FF", "#FFD700"                          // Additional Colors
+      ];
+
+
+
+    return colors[Math.floor(Math.random() * colors.length)];
+}
