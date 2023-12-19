894e87471e6b815dd49d49a0363305bdd8baa977
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2023-12-19 17:40:54 +0100

    PageEfficientBowyer: load from file

--- scripts/PageEfficientBowyerWatson.js
+++ scripts/PageEfficientBowyerWatson.js
@@ -9,6 +9,7 @@ const buttonSkipDemo = document.getElementById("button-skip-demo");
 const buttonRestartDemo = document.getElementById("button-restart-demo");
 const buttonDownloadGif = document.getElementById("button-download-gif");
 const rangeDemoSpeed = document.getElementById("range-demo-speed");
+const fileNodes = document.getElementById("file-nodes");
 
 function recreateCanvas(delay, gif=false) {
     if (canvasTriangulation) {
@@ -17,7 +18,7 @@ function recreateCanvas(delay, gif=false) {
     canvasTriangulation = document.createElement("canvas");
 
     resizeCanvas(canvasTriangulation, Math.min(window.innerWidth, window.innerHeight) - 120, Math.min(window.innerWidth, window.innerHeight) - 120);
-    window.addEventListener("resize", () => resizeCanvas(canvasTriangulation, Math.min(window.innerWidth, window.innerHeight) - 80, Math.min(window.innerWidth, window.innerHeight) - 80));
+    window.addEventListener("resize", () => resizeCanvas(canvasTriangulation, Math.min(window.innerWidth, window.innerHeight) - 120, Math.min(window.innerWidth, window.innerHeight) - 120));
 
     triangulation = new Canvas(nodeData, EfficientBowyerWatson, canvasTriangulation);
     sectionCanvas.appendChild(canvasTriangulation);
@@ -32,3 +33,14 @@ buttonRestartDemo.addEventListener("click", () => recreateCanvas(rangeDemoSpeed.
 buttonDownloadGif.addEventListener("click", () => recreateCanvas(rangeDemoSpeed.value, true));
 
 rangeDemoSpeed.addEventListener("change", () => recreateCanvas(rangeDemoSpeed.value));
+
+fileNodes.addEventListener("change", () => {
+    const reader = new FileReader();
+    reader.addEventListener("load", (event) => {
+        const regex = /\[([\d.,\s]+)\]/g;
+        const matches = event.target.result.match(regex);
+        nodeData = JSON.parse("[" + matches.join(",") + "]");
+        recreateCanvas(rangeDemoSpeed.value);
+    })
+    reader.readAsText(fileNodes.files[0]);
+});
