6c50a38a563e091d15e59664e6a72e2dc050589c
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2023-12-20 17:01:32 +0100

    index: change demo

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-const canvasTriangulation = document.getElementById("canvas-triangulation");
-
-
-const size = Math.min(window.innerWidth, window.innerHeight) - 80;
-resizeCanvas(canvasTriangulation, size, size);
-window.addEventListener("resize", () => resizeCanvas(canvasTriangulation, Math.min(window.innerWidth, window.innerHeight) - 80, Math.min(window.innerWidth, window.innerHeight) - 80));
-
-const triangulation = new Canvas(nodeData, NaiveBowyerWatson, canvasTriangulation);
-triangulation.start();
